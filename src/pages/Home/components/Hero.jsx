2373ba6a873e73c21e0feaef47b87b7d8fff37e4
T3nk4I <66106584+T3nk4I@users.noreply.github.com>
2025-11-24 08:36:35 -0600

    Update event year and dates in Hero component

--- src/pages/Home/components/Hero.jsx
+++ src/pages/Home/components/Hero.jsx
@@ -9,15 +9,15 @@ export default function Hero () {
     <div className='w-full mb-4 bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] to-black from-bg_accent_dark/70 md:mb-6 lg:mb-8'>
       <section className='container mx-auto px-4'>
         <h1 className='w-full mt-14 text-center text-5xl font-extrabold bg-clip-text text-transparent bg-gradient-to-br from-txt_white to-txt_white_purple md:mt-0 md:pt-16 md:text-6xl lg:pt-20 lg:text-7xl xl:text-8xl xl:pt-24'>
-          Xmas{' '}
+          Xmas 8{' '}
           <span className='bg-clip-text text-transparent bg-gradient-to-tr from-accent to-secondary'>
-            2024
+            2025
           </span>
         </h1>
         <h3 className='w-full mt-3 text-center font-semibold text-xs text-primary_dark/75 md:mt-4 md:text-sm lg:mt-6 lg:text-base xl:mt-8 xl:text-lg'>
           Nuestro evento mas importante del año <br />
           <h4 className='mt-1 font-bold text-primary_dark/85'>
-            4 y 5 de Diciembre
+            3 y 4 de Diciembre
           </h4>
         </h3>
         <div className='flex flex-col w-fit mt-12 mx-auto text-center font-medium text-sm md:flex-row md:w-1/3 md:text-base md:mt-16 lg:text-lg lg:mt-24 xl:text-xl xl:mt-28'>
